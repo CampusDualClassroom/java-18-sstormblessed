52d7342e1319aa05db6a5a2ceab73cd97f81d005
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-09-26 13:54:33 +0200

    ejercicio completo

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -2,7 +2,24 @@ package com.campusdual.classroom;
 
 public class Exercise18 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	}
+    }
+
+    public static int[] createAndInitializeArray(int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = (i + 1);
+        }
+        return array;
+    }
+
+    public static void showInlineArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+            if (i < array.length - 1) {
+                System.out.println(" ");
+            }
+        }
+    }
 }
